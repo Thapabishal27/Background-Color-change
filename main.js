83873828b74aea03700a6a30ef92e2a406fa90b5
Thapabishal27 <105634518+Thapabishal27@users.noreply.github.com>
2022-05-20 14:02:50 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+let btndown1 = document.getElementsByClassName("btn-nbr1")[0];
+            btndown1.addEventListener("click", function () {
+                document.body.style.backgroundColor = "red";
+                document.body.style.color = "white";
+                document.getElementById("12").style.color = "white";
+                document.getElementById("13").style.color = "white";
+                document.getElementById("14").style.color = "white";
+                document.getElementById("15").style.color = "white";
+
+
+            });
+
+            let btndown2 = document.getElementsByClassName("btn-nbr2")[0];
+            btndown2.addEventListener("click", function () {
+                document.body.style.backgroundColor = "blue";
+                document.body.style.color = "white";
+                document.getElementById("12").style.color = "white";
+                document.getElementById("13").style.color = "white";
+                document.getElementById("14").style.color = "white";
+                document.getElementById("15").style.color = "white";
+
+            });
+
+            let btndown3 = document.getElementsByClassName("btn-nbr3")[0];
+            btndown3.addEventListener("click", function () {
+                document.body.style.backgroundColor = "yellow";
+                document.body.style.color = "black";
+                document.getElementById("12").style.color = "black";
+                document.getElementById("13").style.color = "black";
+                document.getElementById("14").style.color = "black";
+                document.getElementById("15").style.color = "black";
+                document.getElementById("text1").style.Color = "black";
+                document.getElementById("text2").style.Color = "black";
+
+            });
+
+            let btndown4 = document.getElementsByClassName("btn-nbr4")[0];
+            btndown4.addEventListener("click", function () {
+                document.body.style.backgroundColor = "black";
+                document.body.style.Color = "white";
+                document.getElementById("12").style.color = "white";
+                document.getElementById("13").style.color = "white";
+                document.getElementById("14").style.color = "white";
+                document.getElementById("15").style.color = "white";
+                document.getElementById("details").style.color = "white";
+                document.getElementsByClassName("btn-nbr4").style.backgroundColor = "white";
+                document.getElementById("bg-color").style.color = "white";
+            });
